f67f569c17d173c24e84bc5858dc38f6c1600066
Catherine <catherine@Dead-Inside.local>
2020-02-01 12:31:45 -0800

    starter files

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Profile from './Profile.js';
+import Signin from './Signin.js';
+import {
+  UserSession,
+  AppConfig
+} from 'blockstack';
+
+const appConfig = new AppConfig()
+const userSession = new UserSession({ appConfig: appConfig })
+
+export default class App extends Component {
+
+
+  handleSignIn(e) {
+    e.preventDefault();
+    userSession.redirectToSignIn(); // we should edit this to redirect to mail page
+    //userSession.redirectToYourMail();
+  }
+
+  redirectToLoginPage(){
+    
+  }
+
+  handleSignOut(e) {
+    e.preventDefault();
+    userSession.signUserOut(window.location.origin);
+  }
+
+  render() { //checks if user is signed in
+    return (
+      <div className="site-wrapper">
+        <div className="site-wrapper-inner">
+          { !userSession.isUserSignedIn() ?
+            <Signin userSession={userSession} handleSignIn={ this.handleSignIn } />
+            : <Profile userSession={userSession} handleSignOut={ this.handleSignOut } />
+          }
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        window.history.replaceState({}, document.title, "/")
+        this.setState({ userData: userData})
+      });
+    }
+  }
+}
