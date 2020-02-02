96916020494c81d24b5cad914f757b128cf45727
Catherine <catherine@Dead-Inside.local>
2020-02-02 00:26:09 -0800

    can sign up, working on get name from input and signin

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import {
   Person, handlePendingSignIn,
 } from 'blockstack';
-import Webcam from "react-webcam";
 import { signInputs } from 'blockstack/lib/operations';
+
+import Webcam from "react-webcam";
+import { create, all } from 'mathjs'
+
+const config = { }
+const math = create(all, config)
 
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 const {Storage} = require('@google-cloud/storage');
+
+var currentId = 0; 
+const options = { decrypt: true };
 
 export default class Profile extends Component {
   constructor(props) {
@@ -24,8 +32,12 @@ export default class Profile extends Component {
     };
     
     this.webcam = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleSubmit(){
+
+  }
   render() {
     const { handleSignOut, userSession } = this.props;
     const { person } = this.state;
@@ -53,14 +65,23 @@ export default class Profile extends Component {
           />
         </div>
 
+      <form>
+        <label>
+          Full Name:
+          <input type="text" name={this.state.fullName} />
+        </label>
+      </form>
+
         <p className="lead">
           <button
             className="btn btn-primary btn-lg"
-            id="signout-button"
+            id="signup-button"
             // onClick={handleSignOut.bind(this)} // CHANGE ON CLICK
             onClick={()=>{
-              console.log("hi");
-              this.uploadImage();}}
+              // console.log(this.state.fullName);
+              var name = "test name";
+              this.signUp(userSession,this.webcam.current.getScreenshot(),name);
+            }}
           >
             Sign Up
           </button>
@@ -70,15 +91,16 @@ export default class Profile extends Component {
             id="signin-button"
             onClick={()=>{
               console.log('sign in');
-              this.uploadImage(userSession,this.webcam.current.getScreenshot()); //webcam returns base encoded x64 
-            } //TODO: send this image to Gaia
-            } //CHANGE ON CLICK
+              // console.log("name")
+              var name = "test name";
+              this.signIn(userSession,this.webcam.current.getScreenshot(),name); //webcam returns base encoded x64 
+            } 
+            } 
             
           >
             Sign In
           </button>
         </p>  
-      {/* <button onClick={WebcamCapture.capture}>Capture photo</button> */}
       </div> : null
     );
   }
@@ -91,48 +113,103 @@ export default class Profile extends Component {
   }
 
 
- uploadImage(userSession,image) { //later has param image and is called in gcloud
-    // const storage = new Storage();
+ signUp(userSession,image,name) {
+   //set up 
 
-    console.log("tried");
-    //same image 
+    this.clear(userSession).then(()=>{
+      console.log("cleared");
+      currentId = 0;
+      return userSession.getFile("nutty.json")
+    })
 
-    const options = { decrypt: true };
-
-    userSession.putFile("image.txt", image, options)
- .then(() => {
-    // message.txt exists now, and has the contents "hello world!".
-    console.log("successfully uploaded");
- })
-
-//  userSession.getFile('image.txt', options)
-//  .then((file) => {
-//    var statuses = file;
-//    console.log(file);
-//  })
-    //store as json -> 
-
-    // this.props.userSession.putFile("test.txt", "helloworld", options);
-
-      // try {
-      //     // Makes an authenticated API request.
-      //     const results =  storage.getBuckets();
-      //     storage.bucket("postalstorage").upload(file, {
-      //       gzip:true,
-      //       metadata:{
-      //         cacheControl: 'public, max-age=31536000',
-      //       },
-      //     });
-      //     console.log("tried2");
-      //     // const [buckets] = results;
-          
-      //   } catch (err) {
-      //     console.error('ERROR:', err);
-      //   }
+    .then((responseData) => {
+      console.log("responseData:",responseData);
+      // var jsonObject = JSON.parse(json);
+      var jsonObject = JSON.parse(responseData);
+      jsonObject.push({
+        fullName: name, 
+        poBox: currentId,
+        documents: [],
+        image: image // **TODO*** 
+      });
+      userSession.putFile("nutty.json",JSON.stringify(jsonObject),options).then(()=>{
+        console.log("added new user with po box of id ", currentId);
+        currentId = currentId+1;
+      })
+    })
   }
+
+  clear(userSession){ //clears 'database'
+    const options = { decrypt: true };
+    var ex = [];
+    var result = JSON.stringify(ex);
+    return userSession.putFile("nutty.json", result, options)
+  }
+
+  signIn(userSession,image, name){
+    // get using name
+    var max = 0;
+    var maxUserId = 0;
+    var json = userSession.getFile("nutty.json").then((responseData) => {
+      // console.log("responseData:",responseData);
+      // var jsonObject = JSON.parse(json);
+      var jsonObject = JSON.parse(responseData);
+      // jsonObject.name
+      for(var user of jsonObject){
+        // console.log(JSON.stringify(user));
+        // console.log(name.toUpperCase() == user.fullName.toUpperCase()); 
+        if(name.toUpperCase() == user.fullName.toUpperCase()){
+          //find matching image
+          // console.log(JSON.parse(atob(user.image)));
+          
+          // if(math.dot(image, user.image) > max){
+          //   maxUserId = user.boxId;
+          //   max = math.dot(image, user.image);
+          // }
+          // console.log(math.dot([2, 4, 1], [2, 2, 3])  );
+        }
+      }
+      
+    })
+    console.log(max);
+    //then get data for all that shit
+    //return id but at some point change that to id (future)
+    // return maxUserId; 
+  }
+
+  getPendingMail(userSession, boxId){
+    //get the text of nutty.txt 
+    //set the PO box to current id in json
+    // set the 
+
+    //get the ID
+    // var id = this.signIn(userSession, image, name); 
+    
+
+    // userSession.getFile("nutty.json").then(() => {
+    //   var jsonObject = JSON.parse(responseData);
+    //   jsonObject.push([{
+    //     fullName: name, 
+    //     image: image, // **TODO*** 
+    //     poBox: currentId,
+    //     documents: []
+    //   }]);
+    // })
+    
+  }
+
+  // need function to send our Id to backend 
+  // get the box number our mail is in from the hardware side 
+  // call getMail(boxId) from hardware side
+  // if getMail(boxId) == 0: you have no pending mail
+  // else: return boxId
 
   uploadMail() {
 
+  }
+
+  viewMail(){
+    // returns mail associated with person's image and name
   }
 
 
